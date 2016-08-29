1a547e883c1d77b50388af018096ce3ad4f77a61
Michael Khalili <Michael@MichaelApproved.com>
2016-08-29 05:15:53 -0400

    Changed hard coded download notification title
    We need this to be an option.

--- src/android/BackgroundDownload.java
+++ src/android/BackgroundDownload.java
@@ -172,7 +172,7 @@ public class BackgroundDownload extends CordovaPlugin {
 
             DownloadManager mgr = (DownloadManager) this.cordova.getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
             DownloadManager.Request request = new DownloadManager.Request(source);
-            request.setTitle("org.apache.cordova.backgroundDownload plugin");
+            request.setTitle("Mixergy app download");
             request.setVisibleInDownloadsUi(false);
 
             // hide notification. Not compatible with current android api.
